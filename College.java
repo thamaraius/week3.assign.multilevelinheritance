1dccb93922bfc3d3d82f9f1fb0826d2175318b39
thamaraius <113008288+thamaraius@users.noreply.github.com>
2022-10-20 21:48:41 +0530

    Add files via upload
    Multi Level Inheritance

---
+++ College.java
@@ -0,0 +1,29 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName(String name) {
+		
+		//name="VIT University";
+		
+		System.out.println("College name is "+name);
+	}
+	
+	public void collegeCode(int collegecode) {
+		
+		//collegecode=222;
+		System.out.println("Collegecode is "+collegecode);
+		
+		
+	}
+	
+	public void collegeRank(int rank) {
+		
+		//rank=8;
+		System.out.println("College rank is"+rank);
+		
+		
+	}
+
+	
+}
---
+++ Department.java
@@ -0,0 +1,15 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College {
+
+	public void deptName(String deptname) {
+		
+		//deptname="IT APPS";
+		System.out.println("Department name is ..."+deptname);
+	}
+	
+	
+	
+}
